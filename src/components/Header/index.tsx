2b92eee6922f152759b5fd7f41fb4e4a9899ee45
takenokoroid <takenokoroid@gmail.com>
2026-03-30 00:53:37 +0900

    feat: :sparkles: Headerコンポーネントを作成した

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header className="border-b border-gray-200 px-2 py-3 sm:px-4">
+      <nav className="mx-auto max-w-prose">
+        <a href="/" className="inline-block px-2 py-1 text-lg font-semibold">
+          Sokokoco
+        </a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
